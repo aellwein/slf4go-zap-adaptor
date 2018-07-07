ec57fb2f2bde7a152aeacca40776f5e97ba8f11b
Alex Ellwein <alex.ellwein@gmail.com>
2018-07-07 17:16:51 +0200

    add basic implementation and tests

---
+++ slf4go_zap_adaptor.go
@@ -0,0 +1,149 @@
+package slf4go_zap_adaptor
+
+import (
+	"errors"
+	"fmt"
+	"github.com/aellwein/slf4go"
+	"go.uber.org/zap"
+)
+
+type zapLoggerFactory struct {
+	logger *zap.Logger
+}
+
+// Creates by default a production sugared logger
+func (f *zapLoggerFactory) GetLogger(name string) slf4go.Logger {
+	logger := &zapLogger{}
+	logger.sugar = f.logger.Sugar()
+	logger.LoggerAdaptor.SetLevel(slf4go.LevelInfo)
+	return logger
+}
+
+type zapLogger struct {
+	slf4go.LoggerAdaptor
+	sugar *zap.SugaredLogger
+}
+
+func (l *zapLogger) Trace(args ...interface{}) {
+	// trace maps to debug
+	if l.LoggerAdaptor.GetLevel() <= slf4go.LevelTrace {
+		l.sugar.Debug(args...)
+	}
+}
+
+func (l *zapLogger) Tracef(format string, args ...interface{}) {
+	// trace maps to debug
+	if l.LoggerAdaptor.GetLevel() <= slf4go.LevelTrace {
+		l.sugar.Debugf(format, args...)
+	}
+}
+
+func (l *zapLogger) Debug(args ...interface{}) {
+	if l.LoggerAdaptor.GetLevel() <= slf4go.LevelDebug {
+		l.sugar.Debug(args...)
+	}
+}
+
+func (l *zapLogger) Debugf(format string, args ...interface{}) {
+	if l.LoggerAdaptor.GetLevel() <= slf4go.LevelDebug {
+		l.sugar.Debugf(format, args...)
+	}
+}
+
+func (l *zapLogger) Info(args ...interface{}) {
+	if l.LoggerAdaptor.GetLevel() <= slf4go.LevelInfo {
+		l.sugar.Info(args...)
+	}
+}
+
+func (l *zapLogger) Infof(format string, args ...interface{}) {
+	if l.LoggerAdaptor.GetLevel() <= slf4go.LevelInfo {
+		l.sugar.Infof(format, args...)
+	}
+}
+
+func (l *zapLogger) Warn(args ...interface{}) {
+	if l.LoggerAdaptor.GetLevel() <= slf4go.LevelWarn {
+		l.sugar.Warn(args...)
+	}
+}
+
+func (l *zapLogger) Warnf(format string, args ...interface{}) {
+	if l.LoggerAdaptor.GetLevel() <= slf4go.LevelWarn {
+		l.sugar.Warnf(format, args...)
+	}
+}
+
+func (l *zapLogger) Error(args ...interface{}) {
+	if l.LoggerAdaptor.GetLevel() <= slf4go.LevelError {
+		l.sugar.Error(args...)
+	}
+}
+
+func (l *zapLogger) Errorf(format string, args ...interface{}) {
+	if l.LoggerAdaptor.GetLevel() <= slf4go.LevelError {
+		l.sugar.Errorf(format, args...)
+	}
+}
+
+func (l *zapLogger) Fatal(args ...interface{}) {
+	if l.LoggerAdaptor.GetLevel() <= slf4go.LevelFatal {
+		l.sugar.Fatal(args...)
+	}
+}
+
+func (l *zapLogger) Fatalf(format string, args ...interface{}) {
+	if l.LoggerAdaptor.GetLevel() <= slf4go.LevelFatal {
+		l.sugar.Fatalf(format, args...)
+	}
+}
+
+func (l *zapLogger) Panic(args ...interface{}) {
+	if l.LoggerAdaptor.GetLevel() <= slf4go.LevelPanic {
+		l.sugar.Panic(args...)
+	}
+}
+
+func (l *zapLogger) Panicf(format string, args ...interface{}) {
+	if l.LoggerAdaptor.GetLevel() <= slf4go.LevelPanic {
+		l.sugar.Panicf(format, args...)
+	}
+}
+
+func newZapLoggerFactory() slf4go.LoggerFactory {
+	factory := &zapLoggerFactory{}
+	factory.logger, _ = zap.NewProduction()
+	return factory
+}
+
+func (f *zapLoggerFactory) SetLoggingParameters(params slf4go.LoggingParameters) error {
+	for k, v := range params {
+		switch k {
+		case "development":
+			if dev, ok := v.(bool); !ok {
+				return errors.New("invalid type for parameter 'development', should be of type bool")
+			} else {
+				if dev {
+					f.logger, _ = zap.NewDevelopment()
+				}
+			}
+
+		case "options":
+			if options, ok := v.([]zap.Option); !ok {
+				return errors.New("invalid type for parameter 'options', should be of type []zap.Option")
+			} else {
+				f.logger = f.logger.WithOptions(options...)
+			}
+
+		case "fields":
+			if fields, ok := v.([]zap.Field); !ok {
+				return errors.New("invalid type for parameter 'fields', should be of type []zap.Field")
+			} else {
+				f.logger = f.logger.With(fields...)
+			}
+		default:
+			return errors.New(fmt.Sprintf("unsupported parameter: %v", k))
+		}
+	}
+	return nil
+}
---
+++ slf4go_zap_adaptor_test.go
@@ -0,0 +1,157 @@
+package slf4go_zap_adaptor
+
+import (
+	"errors"
+	"github.com/aellwein/slf4go"
+	"github.com/bouk/monkey"
+	"github.com/smartystreets/assertions"
+	"go.uber.org/zap"
+	"os"
+	"testing"
+)
+
+func TestGetLogger(t *testing.T) {
+	logger := slf4go.GetLogger("test")
+
+	var levels = []slf4go.LogLevel{
+		slf4go.LevelPanic,
+		slf4go.LevelFatal,
+		slf4go.LevelError,
+		slf4go.LevelWarn,
+		slf4go.LevelInfo,
+		slf4go.LevelDebug,
+		slf4go.LevelTrace,
+	}
+
+	for _, i := range levels {
+		logger.SetLevel(i)
+		logger.Trace("Trace")
+		logger.Tracef("Tracef: %v", logger)
+		logger.Debug("Debug")
+		logger.Debugf("Debugf: %s", "debug mode")
+		logger.Info("Info")
+		logger.Infof("Infof: %v", slf4go.GetLoggerFactory())
+		logger.Warn("Warn")
+		logger.Warnf("Warnf: %d", 42)
+		logger.Error("Error")
+		logger.Errorf("Errorf: %v", errors.New("some error"))
+	}
+}
+
+func TestLoggerFatal(t *testing.T) {
+	mockExit := func(int) {
+		panic("mockExit called")
+	}
+	patch := monkey.Patch(os.Exit, mockExit)
+	defer patch.Unpatch()
+
+	logger := slf4go.GetLogger("test")
+	underTest := func() {
+		logger.Fatal("fatality!")
+	}
+
+	assertions.ShouldPanic(underTest)
+}
+
+func TestLoggerFatalf(t *testing.T) {
+	mockExit := func(int) {
+		panic("mockExit called")
+	}
+	patch := monkey.Patch(os.Exit, mockExit)
+	defer patch.Unpatch()
+
+	logger := slf4go.GetLogger("test")
+	underTest := func() {
+		logger.Fatalf("fatality: %d", 42)
+	}
+
+	assertions.ShouldPanic(underTest)
+}
+
+func TestLoggerPanic(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The code did not panic as expected")
+		}
+	}()
+	logger := slf4go.GetLogger("test")
+	logger.Panic("this is expected to cause panic")
+}
+
+func TestLoggerPanicf(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The code did not panic as expected")
+		}
+	}()
+	logger := slf4go.GetLogger("test")
+	logger.Panicf("this is expected to cause panic: %d", 42)
+}
+
+type ParamTester struct {
+	name      string
+	goodValue interface{}
+	badValue  interface{}
+}
+
+func TestSetLoggingParametersAutomatically(t *testing.T) {
+	var params = []ParamTester{
+		{
+			name:      "development",
+			goodValue: true,
+			badValue:  1337,
+		},
+		{
+			name:      "options",
+			goodValue: []zap.Option{},
+			badValue:  1337,
+		},
+		{
+			name:      "fields",
+			goodValue: []zap.Field{zap.Int("meaning of life", 42)},
+			badValue:  1337,
+		},
+	}
+
+	for _, p := range params {
+		goodTest := func(pt *ParamTester) {
+			prm := slf4go.LoggingParameters{pt.name: pt.goodValue}
+			err := slf4go.GetLoggerFactory().SetLoggingParameters(prm)
+			if err != nil {
+				panic(err)
+			}
+		}
+		badTest := func(pt *ParamTester) {
+			prm := slf4go.LoggingParameters{pt.name: pt.badValue}
+			err := slf4go.GetLoggerFactory().SetLoggingParameters(prm)
+			if err == nil {
+				panic(err)
+			}
+		}
+		assertions.ShouldNotPanic(func() { goodTest(&p) })
+		assertions.ShouldNotPanic(func() { badTest(&p) })
+	}
+
+	// also test an unknown parameter
+	assertions.ShouldPanic(func() {
+		if err := slf4go.GetLoggerFactory().SetLoggingParameters(
+			slf4go.LoggingParameters{"xyzunknown": "blah"}); err != nil {
+			panic(err)
+		}
+	})
+
+	// ...and the branch with no params
+	assertions.ShouldBeNil(slf4go.GetLoggerFactory().SetLoggingParameters(slf4go.LoggingParameters{}))
+	assertions.ShouldBeNil(slf4go.GetLoggerFactory().SetLoggingParameters(nil))
+}
+
+func TestZapLogger_Development(t *testing.T) {
+	slf4go.GetLoggerFactory().SetLoggingParameters(slf4go.LoggingParameters{
+		"development": true,
+		"fields": []zap.Field{
+			zap.Bool("alex", true),
+		},
+	})
+	logger := slf4go.GetLogger("test")
+	logger.Infof("test: %d", 42)
+}
